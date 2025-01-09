774aaedea4b944b1fe3efefa65cb3da0e727ebe3
KhiryDixonManning <Manningkhiry@gmail.com>
2025-01-09 13:41:45 -0600

    made pycodestyle compliant

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -4,11 +4,11 @@
 
 def matrix_shape(matrix):
     """Return the shape (dimensions) of the given matrix."""
-    shape = []  # Initialize an empty list to store the dimensions of the matrix.
+    shape = []  # Initialize empty list to store the dimensions of the matrix.
 
     while isinstance(matrix, list):  # Continue if the matrix is a list.
-        shape.append(len(matrix))  # Add the length of the current dimension to the shape list.
+        shape.append(len(matrix))  # Add length of the current dimension to the shape list.
         matrix = matrix[
-            0] if matrix else []  # Move to the first element of the next dimension, or empty if no further dimensions.
+            0] if matrix else []  # Move to first element of the next dimension, or empty if no further dimensions.
 
     return shape
