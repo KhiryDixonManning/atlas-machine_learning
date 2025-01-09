4cc352ee407f33a339de42534301aa76690f779e
KhiryDixonManning <Manningkhiry@gmail.com>
2025-01-09 15:08:03 -0600

    trying to get task 1 working

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
-the_middle = []  # Creates an empty list as the_middle to store the 2D matrix
-for row in matrix:  # Starts a loop that iterates over each row
-    the_middle.append(row[2:4])  # Extracts elements from the 3rd and 4th
+the_middle = []
+
+# Extract the middle columns (3rd and 4th columns) from each row
+for row in matrix:
+    the_middle.append(row[2:4])
+
 print("The middle columns of the matrix are: {}".format(the_middle))
