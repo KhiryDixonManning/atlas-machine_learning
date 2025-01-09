9f1beccdc0a4e6d72804ff1174a87801bb225149
KhiryDixonManning <Manningkhiry@gmail.com>
2025-01-09 13:32:30 -0600

    task 2 test

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Write a function that calculates the shape of a matrix"""
+
+
+def matrix_shape(matrix):
+    """Return the shape (dimensions) of the given matrix."""
+    shape = []  # Initialize an empty list to store the dimensions of the matrix.
+
+    while isinstance(matrix, list):  # Continue if the matrix is a list.
+        shape.append(len(matrix))  # Add the length of the current dimension to the shape list.
+        matrix = matrix[
+            0] if matrix else []  # Move to the first element of the next dimension, or empty if no further dimensions.
+
+    return shape
