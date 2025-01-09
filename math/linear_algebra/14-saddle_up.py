a7cc0af5a94ada7c7930eade772c04239d7add94
KhiryDixonManning <Manningkhiry@gmail.com>
2025-01-09 15:03:57 -0600

    changed 14 for check

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ matrix mult"""
-import numpy as np  # imports the NumPy library and makes it accessible
+import numpy as np  # gets the NumPy library and makes it accessible
 
 
 def np_matmul(mat1, mat2):  # defines the function np_matmul
