2e2499e2c4752064355ad4c1f901ffeb876d0a85
KhiryDixonManning <Manningkhiry@gmail.com>
2025-01-10 17:55:47 -0600

    checking to see if checker will still ding me after adding comments

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 y = np.arange(0, 11) ** 3
 
 #plot the graph
-plt.xlim(0, 10)
-plt.ylim(0, 11 ** 3)
-plt.plot(y, color='red')
-plt.show()
+plt.xlim(0, 10) #x-axis range is 0-10
+plt.plot(y, color='red') # r makes solid red line
+plt.show() #display
