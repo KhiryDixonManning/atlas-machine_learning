c91fe7e3881eac8e4e6d9cca790708b5b142a88d
KhiryDixonManning <Manningkhiry@gmail.com>
2025-01-10 18:01:57 -0600

    made pycode compliant as well as adjusted graph. It looks exactly like their graph but still wrong? hopefully changing this fixes it.

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 y = np.arange(0, 11) ** 3
 
-#plot the graph
-plt.xlim(0, 10) #x-axis range is 0-10
-plt.plot(y, color='red') # r makes solid red line
-plt.show() #display
+# plot the graph
+plt.xlim(0, 10)  # x-axis range is 0-10
+plt.plot(y, color='red')  # r makes solid red line
+plt.show()  # display
