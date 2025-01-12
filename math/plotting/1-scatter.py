b2f4eda745b2676fbb02895ba0d3e503bfd73746
KhiryDixonManning <Manningkhiry@gmail.com>
2025-01-12 11:27:40 -0600

    all of tasks should work correctly, gave execution, made sure was in pycodestyle

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np  # imports NumPy library and allows you to refer to it
+import matplotlib.pyplot as plt  # plotting library for Python
+
+mean = [69, 0]  # a list mean with two elements
+cov = [[15, 8], [8, 15]]  # a 2x2 list cov
+np.random.seed(5)
+""" generates 2000 random samples from multivariate normal distribution."""
+x, y = np.random.multivariate_normal(mean, cov, 2000).T
+y += 180  # adds 180 to every element in the y array
+
+plt.scatter(x, y, color='m')  # creates a scatter plot using matplotlib
+plt.xlabel('height(in)')  # xaxis is height
+plt.ylabel('weight(lbs)')  # yaxis is weight
+plt.title("Men's Height vs Weight")  # Title of scatter plot
+plt.show()
---
+++ math/plotting/6-bars.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+fruit = np.random.randint(0, 20, (4, 3))
+
+fruit_labels = ['Apples', 'Bananas', 'Oranges', 'Peaches']
+person_labels = ['Farrah', 'Fred', 'Felicia']
+
+colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+
+plt.bar(person_labels, fruit[0], color=colors[0], label=fruit_labels[0])
+for i in range(1, len(fruit_labels)):
+        plt.bar(person_labels, fruit[i], bottom=np.sum(fruit[:i], axis=0), color=colors[i], label=fruit_labels[i])
+
+        plt.xlabel('Person')
+        plt.ylabel('Quantity of Fruit')
+        plt.title('Number of Fruit per Person')
+        plt.ylim(0, 80)
+        plt.yticks(np.arange(0, 81, 10))
+        plt.legend()
+
+        plt.show()
