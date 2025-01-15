8428da0731a2db69000d21cab089b74ec6e070cd
KhiryDixonManning <Manningkhiry@gmail.com>
2025-01-15 14:39:24 -0600

    defined files

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
-# line = __import__('0-line').line
 
-y = np.arange(0, 11) ** 3
+def line():
 
-# plot the graph
-plt.xlim(0, 10)  # x-axis range is 0-10
-plt.plot(y, color='red')  # r makes solid red line
-plt.show()  # display
+    y = np.arange(0, 11) ** 3
+
+    # plot the graph
+    plt.xlim(0, 10)  # x-axis range is 0-10
+    plt.plot(y, color='red')  # r makes solid red line
+    plt.show()  # display
--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -3,15 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 change_scale = __import__('2-change_scale').change_scale
 
-x = np.arange(0, 28651, 5730)
-r = np.log(0.5)
-t = 5730
-y = np.exp((r / t) * x)
+def change_scale():
 
-plt.plot(x, y)
-plt.xlabel('Time (years)')
-plt.ylabel('Fraction Remaining')
-plt.title("Exponential Decay of C-14")
-plt.yscale('log')
-plt.xlim(0, 28650)
-plt.show()
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+
+    plt.plot(x, y)
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title("Exponential Decay of C-14")
+    plt.yscale('log')
+    plt.xlim(0, 28650)
+    plt.show()
