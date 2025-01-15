1c397c4ef11bf92abc1062342633f6df950241ed
KhiryDixonManning <Manningkhiry@gmail.com>
2025-01-15 15:33:08 -0600

    changed frequency as test

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -6,13 +6,19 @@ import matplotlib.pyplot as plt
 def frequency():
 
     np.random.seed(5)
-student_grades = np.random.normal(68, 15, 50)
-bins = range(0, 101, 10)
+    student_grades = np.random.normal(68, 15, 50)
+    bins = range(0, 101, 10)
 
-plt.hist(student_grades, bins=bins, edgecolor='black')
-plt.xlabel('Grades')
-plt.ylabel("Number of Students")
-plt.title("Project A")
-plt.xlim(0, 100)
-plt.ylim(0, 30)
-plt.show()
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+    plt.xlabel('Grades')
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+    plt.show()
+
+    print("Plot generated successfully!")  # This is 32 characters long
+
+
+# Call the function to generate the plot
+frequency()
