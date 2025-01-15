3f948b411984d3038cac4b60ae2887e3128eb4f1
KhiryDixonManning <Manningkhiry@gmail.com>
2025-01-15 15:29:03 -0600

    made graphs display correctly

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 def frequency():
 
     np.random.seed(5)
-    student_grades = np.random.normal(68, 15, 50)
-    bins = range(0, 101, 10)
+student_grades = np.random.normal(68, 15, 50)
+bins = range(0, 101, 10)
 
-    plt.hist(student_grades, bins=bins, edgecolor='black')
-    plt.xlabel('Grades')
-    plt.ylabel("Number of Students")
-    plt.title("Project A")
-    plt.xlim(0, 100)
-    plt.ylim(0, 30)
-    plt.show()
+plt.hist(student_grades, bins=bins, edgecolor='black')
+plt.xlabel('Grades')
+plt.ylabel("Number of Students")
+plt.title("Project A")
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+plt.show()
--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 
 def bars():
-
         np.random.seed(5)
 fruit = np.random.randint(0, 20, (4, 3))
 
@@ -17,11 +16,11 @@ plt.bar(person_labels, fruit[0], color=colors[0], label=fruit_labels[0])
 for i in range(1, len(fruit_labels)):
         plt.bar(person_labels, fruit[i], bottom=np.sum(fruit[:i], axis=0), color=colors[i], label=fruit_labels[i])
 
-        plt.xlabel('Person')
-        plt.ylabel('Quantity of Fruit')
-        plt.title('Number of Fruit per Person')
-        plt.ylim(0, 80)
-        plt.yticks(np.arange(0, 81, 10))
-        plt.legend()
+plt.xlabel('Person')
+plt.ylabel('Quantity of Fruit')
+plt.title('Number of Fruit per Person')
+plt.ylim(0, 80)
+plt.yticks(np.arange(0, 81, 10))
+plt.legend()
 
-        plt.show()
+plt.show()
