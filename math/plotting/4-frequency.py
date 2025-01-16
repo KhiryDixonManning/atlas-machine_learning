136a2b76dcbd222a3a3d53ec8853bef2417f416b
KhiryDixonManning <Manningkhiry@gmail.com>
2025-01-16 11:24:03 -0600

    test 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -6,25 +6,22 @@ import matplotlib.pyplot as plt
 def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
     bins = range(0, 101, 10)
 
-    plt.hist(student_grades, bins=bins, edgecolor='black')
+    plt.hist(student_grades, bins=bins, edgecolor='k')
     plt.xlabel('Grades')
     plt.ylabel("Number of Students")
     plt.title("Project A")
 
     # Set x-axis ticks to count by 10s (from 0 to 100)
-    plt.xticks(range(0, 101, 10))
+    plt.xticks(np.arange(0, 101, 10))
 
     # Set y-axis ticks to count by 5s (from 0 to 30)
-    plt.yticks(range(0, 31, 5))
+    plt.yticks(np.arange(0, 31, 5))
 
     # Set axis limits
     plt.xlim(0, 100)
     plt.ylim(0, 30)
 
     plt.show()
-
-
-# Call the frequency function to display the plot
-frequency()
