8e0fc7e303f4ac0bc9fdcecad4da0ce4e2bf5c5e
KhiryDixonManning <Manningkhiry@gmail.com>
2025-02-19 12:35:17 -0600

    tensorflow test

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
-import tensorflow as tf
+""" module containing function that creates a layer of the tensorflow NN """
+import tensorflow.compat.v1 as tf
+
 
 def create_layer(prev, n, activation):
     """ function that creates a layer of the tensorflow NN
 
-    parameters:
-        prev[tensor] - the tensor output of the previous layer
-        n [int] - the number of nodes in the layer to create
-        activation - [?] the activation function that they should use
+        Parameters:
+            prev [tensor] - the tensor output of the previous layer
+            n [int] - the number of nodes in the layer to create
+            activation [?] - the activation function that the layer should use
 
-    returns:
-        [tensor] tensor output for layer
-    """
-
+        Returns:
+            [tensor] tensor output for the layer
+        """
     init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
-        name='layer',
+        name="layer",
         kernel_initializer=init
     )
     return layer(prev)
