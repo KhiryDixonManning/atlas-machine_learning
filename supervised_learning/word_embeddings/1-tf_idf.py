a9a012bb850291f2d82bcb11dabfbb1c49b83ce1
KhiryDixonManning <Manningkhiry@gmail.com>
2025-12-09 17:03:28 -0600

    word embeddings

---
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+"""shebang line for tf_idf"""
+import numpy as np
+import tensorflow.keras as keras
+
+
+def tf_idf(sentences, vocab=None):
+    """Function that creates a TF-IDF embedding
+        sentences: a built list of sentences to analyze
+        vocab: a list of the vocab words to use for the analysis
+            If None, all words within sentences should be used
+        Returns: embeddings, features
+            embeddings is a numpy.ndarray of shape (s, f) containing the
+            embeddings
+                s is the number of sentences in sentences
+                f is the number of features analyzed
+            features is a list of the features used for embeddings"""
+
+    # tokenize sentences
+    sentence_tokenized = [tokenize(sentence) for sentence in sentences]
+
+    # if no vocab is provided, create one from all unique words
+    if vocab is None:
+        # Take out all unique words and sort
+        vocab = sorted(set(word for tokens in sentence_tokenized for word in
+                           tokens))
+
+        vocab = np.array(vocab)
+
+    # Document frequency for each word
+    document_frequency = {word: sum(1 for tokens in sentence_tokenized if word
+                                    in tokens) for word in vocab}
+
+    # Initialize the embeddings matrix
+    embeddings = np.zeros((len(sentences), len(vocab)))
+
+    # Calculate TF - IDF for each word in sentence
+    for i, tokens in enumerate(sentence_tokenized):
+        # Skip empty sentences
+        if not tokens:
+            continue
+
+        # Calculation of term freq for each word in current sentence
+        # TF = (occurences of term) / (total terms in doc)
+        frequency = {token: tokens.count(token) / len(tokens) for token in
+                     tokens if token in vocab}
+
+        # Calculate TF - IDF for each word in vocab
+        for j, word in enumerate(vocab):
+            if word in frequency:
+                # Term frequency
+                tf = frequency[word]
+
+                # Inverse document frequency (with smoothing)
+                # Adding 1 to numerator and denominator to prevent division by
+                # zero and reduce impact of rare terms
+                idf = np.log((1 + len(sentences)) /
+                             (1 + document_frequency[word])) + 1
+
+                # TF - IDF score
+                embeddings[i, j] = tf * idf
+
+    # Normalize using L2 normalization
+    # Ensures all documents are comarable regardless of length
+    norm = np.linalg.norm(embeddings, axis=1, keepdims=True)
+    embeddings = np.divide(embeddings, norm, where=norm != 0)
+
+    return embeddings, np.array(vocab)
+
+
+def tokenize(sentence):
+    """tokenization of sentence in to words"""
+
+    # Convert all capital letters to lowercase so they match
+    sentence = sentence.lower()
+    sentence = sentence.replace("'s", "")
+
+    # Remove punctuation
+    for char in "{}()[];:\"'.,!?":
+        sentence = sentence.replace(char, "")
+    # Split into words
+    return sentence.split()
---
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""module for fastext"""
+import gensim
+
+
+def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
+                   window=5, cbow=True, epochs=5, seed=0, workers=1):
+    """Function that creates, builds and trains a genism fastText model
+        sentences: list of sentences to be trained on
+        vector_size: dimensionality of the embedding layer
+        min_count: min number of occurrences of a word for use in training
+        window: max distance between current and predicted word within sentence
+        negative: size of negative sampling
+        cbow: a boolean to determine the training type;
+            True is for CBOW
+            False is for Skip-gram
+        epoch: number of iterations to train over
+        seed: seed for the random number generator
+        workers: number of worker threads to train the model
+    Returns: the trained model"""
+    # fastText model with specific parameters
+    model = gensim.models.FastText(
+        sentences,
+        vector_size=vector_size,
+        window=window,
+        min_count=min_count,
+        negative=negative,
+        seed=seed,
+        workers=workers,
+        epochs=epochs,
+        sg=not cbow
+    )
+    # Build the vocab from sentences
+    model.build_vocab(sentences)
+    # Train model on the input sentences
+    model.train(
+        sentences,
+        total_examples=model.corpus_count,
+        epochs=epochs
+    )
+    return model
